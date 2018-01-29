413d115e045adc3f0e859b3cd54513b1ef4e86f3
Dan Florio <dflo@danflorio.com>
2018-01-29 15:56:35 -0500

    begin styling

--- src/app/App.js
+++ src/app/App.js
@@ -8,10 +8,24 @@ class App extends Component {
     return (
       <div className="App">
         <div className="top-bar">
-          <FontAwesomeIcon className="top-bar__cart" icon={faShoppingCart} />
+          <a className="top-bar__cart">
+            <FontAwesomeIcon className="top-bar__cart-icon" icon={faShoppingCart} />
+            CART (0)
+          </a>
         </div>
         <section className="hero">
-        
+          <div className="hero__overlay"></div>
+          <div className='hero__intro'>
+            <h1 className="hero__title">
+              paintcheap
+            </h1>
+            <p className="hero__text">
+              We put art in the hands of those who need it most.
+            </p>
+            <a className="hero__button">
+              FIND A PAINTING
+            </a>
+          </div>
         </section>
       </div>
     );
