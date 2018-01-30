b836778426b108e481d06cbc2a538dadc5fa4dfc
Dan Florio <dflo@danflorio.com>
2018-01-29 23:05:14 -0500

    refactor code

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App__overlay"></div>
         <TopBar />
         <Home reviews={reviews}/>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,17 +9,15 @@ class Home extends Component {
 
   render() {
     return (
-      <section className="hero">
-        <div className="hero__overlay"></div>
-        <div className="home__center">
-        <div className='hero__intro'>
-          <h1 className="hero__title">
+      <section className="home">
+        <div className="home__intro">
+          <h1 className="home__title">
             paintcheap
           </h1>
-          <p className="hero__text">
+          <p className="home__text">
             We put art in the hands of those who need it most.
           </p>
-          <a className="hero__button">
+          <a className="home__button">
             FIND A PAINTING
           </a>
         </div>
@@ -46,7 +44,6 @@ class Home extends Component {
             )
           }
         </Slider>
-        </div>
       </section>
     );
   }
