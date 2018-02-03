16fb501e3543367a440f784a92152cd26438d588
Dan Florio <dflo@danflorio.com>
2018-02-03 11:32:07 -0500

    specify proptypes for top bar

--- src/parts/top-bar/TopBar.js
+++ src/parts/top-bar/TopBar.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/fontawesome-free-solid';
 import { NavLink } from 'react-router-dom';
 import './TopBar.css';
 
-const TopBar = (props) => {
+const TopBar = ({backgroundVisible, cartTotal}) => {
   return (
-    <div className={"top-bar" + (props.backgroundVisible ? ' top-bar--opaque' : '')}>
+    <div className={"top-bar" + (backgroundVisible ? ' top-bar--opaque' : '')}>
       <NavLink className="top-bar__home" to='/'>PaintCheap</NavLink>
       <a className="top-bar__cart">
         <FontAwesomeIcon className="top-bar__cart-icon" icon={faShoppingCart} />
-        CART ({props.cartTotal})
+        CART ({cartTotal})
       </a>
     </div>
   );
+}
+
+TopBar.propTypes = {
+  backgroundVisible: PropTypes.bool,
+  cartTotal: PropTypes.number
 }
 
 export default TopBar;
