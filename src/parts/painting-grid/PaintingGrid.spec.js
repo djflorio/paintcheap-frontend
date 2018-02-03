4d06c1530aafc0e1902d219262d748528fdc6fa8
Dan Florio <dflo@danflorio.com>
2018-02-02 22:34:54 -0500

    set inCart when addToOrRemoveFromCart is triggered

--- src/parts/painting-grid/PaintingGrid.spec.js
+++ src/parts/painting-grid/PaintingGrid.spec.js
@@ -1,6 +1,7 @@
 import reducer from './PaintingGridReducer';
 import paintings from '../../dummyData/paintings';
 import * as actions from './PaintingGridActions';
+import { addToOrRemoveFromCart } from '../cart/CartActions';
 
 describe('PaintingGrid', () => {
 
@@ -20,6 +21,28 @@ describe('PaintingGrid', () => {
   
   it('should return paintings when fetch is fulfilled', () => {
     expect(reducer(undefined, actions.fetchPaintings())).toEqual({ paintings });
+  });
+
+  it('should set inCart to true if false with addToOrRemoveFromCart', () => {
+    const paintings2 = paintings.slice();
+    paintings2[1] = Object.assign({}, paintings2[1], {
+      inCart: true
+    });
+    expect(reducer(
+      {paintings: paintings},
+      addToOrRemoveFromCart(paintings[1]))
+    ).toEqual({paintings: paintings2});
+  });
+
+  it('should set inCart to false if true with addToOrRemoveFromCart', () => {
+    const paintings2 = paintings.slice();
+    paintings2[1] = Object.assign({}, paintings2[1], {
+      inCart: true
+    });
+    expect(reducer(
+      {paintings: paintings2},
+      addToOrRemoveFromCart(paintings[1]))
+    ).toEqual({paintings: paintings});
   });
   
 });
--- src/parts/painting-grid/PaintingGridReducer.js
+++ src/parts/painting-grid/PaintingGridReducer.js
@@ -1,6 +1,5 @@
-import {
-    FETCH_PAINTINGS_FULFILLED
-} from './PaintingGridActions';
+import { FETCH_PAINTINGS_FULFILLED } from './PaintingGridActions';
+import { ADD_TO_OR_REMOVE_FROM_CART } from '../cart/CartActions';
 
 const paintingGrid = (state={paintings: []}, action) => {
     switch(action.type) {
@@ -8,6 +7,21 @@ const paintingGrid = (state={paintings: []}, action) => {
             return Object.assign({}, state, {
                 paintings: action.payload
             });
+        }
+        case ADD_TO_OR_REMOVE_FROM_CART: {
+            for (let i=0; i < state.paintings.length; i++) {
+                if (state.paintings[i].id == action.painting.id) {
+
+                    const newPaintings = state.paintings.slice();
+                    newPaintings[i] = Object.assign({}, newPaintings[i], {
+                        inCart: !newPaintings[i].inCart
+                    });
+                    
+                    return Object.assign({}, state, {
+                        paintings: newPaintings
+                    });
+                }
+            }
         }
         default:
             return state;
